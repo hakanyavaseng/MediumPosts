e732844f47f2f301e1df9e0332e222c68114441e
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-02-03 20:59:42 +0300

    post01

---
+++ 01_Persisting-Data/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine();
+
+
+
+#region Entity
+class Person
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Address { get; set; }
+}
+#endregion
+
+#region DbContext
+class PersonDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set;}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer(@"Server =.\SQLEXPRESS;Database =PersonDb;Trusted_Connection = True;TrustServerCertificate=True;");
+	}
+}
+#endregion
+
